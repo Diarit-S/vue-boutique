ea6b37c5b8ba319c0b47a02f11daecc07b7fb933
Diarit-S <diarit-94@hotmail.fr>
2019-07-22 09:52:22 +0200

    continue the cours

--- src/components/features/User/Shop/ShopProductsItem.vue
+++ src/components/features/User/Shop/ShopProductsItem.vue
@@ -10,7 +10,7 @@
       <p>{{product.description}}</p>
     </div>
     <div>
-      <span>{{product.price}} €</span>
+      <span>{{product.price | price}}</span>
       <button @click="addProductToCart" class="btn btn-primary btn-sm float-right">Commander</button>
     </div>
   </div>
